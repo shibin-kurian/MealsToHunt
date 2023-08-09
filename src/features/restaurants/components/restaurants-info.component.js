156946371d7ed36a651b87ca055dabf9d815f338
shibin-kurian <shibinkurian2013@gmail.com>
2023-08-09 21:38:03 +0530

    first commit

---
+++ src/features/restaurants/components/restaurants-info.component.js
@@ -0,0 +1,41 @@
+import React from "react";
+import styled from "styled-components/native";
+import { Text, StyleSheet } from "react-native";
+import { Card } from "react-native-paper";
+
+const Title = styled.Text`
+  padding: 16px;
+  color: red;
+`;
+const RestaurantCard = styled(Card)`
+  backgroundcolor: "white";
+`;
+const RestaurantCardCover = styled(Card.Cover)`
+  padding: 20px;
+  backgroundcolor: "white";
+`;
+export const RestaurantInfo = ({ restaurant = {} }) => {
+  const {
+    name = "Some Restaurant",
+    icon,
+    photos = [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+    ],
+    address = "100 some random street",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily,
+  } = restaurant;
+
+  return (
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      <Title>{name}</Title>
+    </RestaurantCard>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: { backgroundColor: "white" },
+  cover: { padding: 20, backgroundColor: "white" },
+});
---
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -0,0 +1,44 @@
+import React from "react";
+import styled from "styled-components/native";
+import { Searchbar } from "react-native-paper";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  StatusBar,
+} from "react-native";
+import { RestaurantInfo } from "../components/restaurants-info.component";
+const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
+`;
+const SearchContainer = styled.View`
+  padding: 16px;
+  background-color: "green";
+`;
+const RestaurantListContainer = styled.View`
+  flex: 1;
+  padding: 16px;
+  background-color: blue;
+`;
+export const RestaurantsScreen = () => (
+  <SafeArea>
+    <SearchContainer>
+      <Searchbar />
+    </SearchContainer>
+
+    <RestaurantListContainer>
+      <RestaurantInfo />
+    </RestaurantListContainer>
+  </SafeArea>
+);
+
+const styles = StyleSheet.create({
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "blue",
+  },
+});
