9e18ff2140fe4ee16c0d6d04ca76d06a1a745aa8
shibin-kurian <shibinkurian2013@gmail.com>
2023-10-18 17:58:15 +0530

    map screen and adding separate search component

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,4 +1,16 @@
 import React, { useContext, useState, useEffect } from "react";
+import styled from "styled-components/native";
 import MapView from "react-native-maps";
+import { Search } from "../components/search.component";
 
-export const MapScreen = () => <MapView style={{ height: "100%" }} />;
+const Map = styled(MapView)`
+  height: 100%;
+  width: 100%;
+`;
+
+export const MapScreen = () => (
+  <>
+    <Search />
+    <Map />
+  </>
+);
