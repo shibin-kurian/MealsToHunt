1415b5a97e5556d5ff457074a65ef790249e4a20
shibin-kurian <shibinkurian2013@gmail.com>
2023-10-20 23:07:24 +0530

    hooking up favourites with favourite hook and contextprovider

--- src/features/restaurants/components/restaurants-info.component.js
+++ src/features/restaurants/components/restaurants-info.component.js
@@ -36,7 +36,7 @@ export const RestaurantInfo = ({ restaurant = {} }) => {
   return (
     <RestaurantCard elevation={5}>
       <View>
-        <Favourite />
+        <Favourite restaurant={restaurant} />
         <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
       </View>
       <Info>
