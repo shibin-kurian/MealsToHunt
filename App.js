fc36dc623078fea9293b1634777ff5a17b6a49d0
shibin-kurian <shibinkurian2013@gmail.com>
2023-10-21 18:36:15 +0530

    firebase installation

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { NavigationLogic } from "./src/infrastructure/navigation/index";
 import React from "react";
 import { ThemeProvider } from "styled-components/native";
+import { initializeApp } from "firebase/app";
 
 import {
   useFonts as useOswald,
@@ -13,6 +14,17 @@ import { theme } from "./src/infrastructure/theme";
 import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
 import { LocationContextProvider } from "./src/services/location/location.context";
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA1LruQxbkPZLPLAR7blIp3fbadiDgdSO0",
+  authDomain: "mealstohunt.firebaseapp.com",
+  projectId: "mealstohunt",
+  storageBucket: "mealstohunt.appspot.com",
+  messagingSenderId: "1090177879394",
+  appId: "1:1090177879394:web:7f7f9688ba645051cc703b",
+};
+
+initializeApp(firebaseConfig);
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
