f2ed7e7ec4f4b4ed17da9eef8a12b1d94fdfbf1f
shibin-kurian <shibinkurian2013@gmail.com>
2023-10-24 19:51:22 +0530

    logout function and user login session consistency over reload

--- src/infrastructure/navigation/app.navigator.js
+++ src/infrastructure/navigation/app.navigator.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import { RestaurantsNavigator } from "./restaurants.navigator";
 import { SafeArea } from "../../components/utility/safe-area.component";
 import { Ionicons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { MapScreen } from "../../features/map/screens/map.screen";
-import { Text } from "react-native";
+import { Text, Button } from "react-native";
+import { AuthenticationContext } from "../../services/authentication/authentication.context";
 
 const Tab = createBottomTabNavigator();
 
@@ -13,12 +14,15 @@ const TAB_ICON = {
   Map: "md-map-sharp",
   Settings: "construct-sharp",
 };
-
-const Settings = () => (
-  <SafeArea>
-    <Text>Settings</Text>
-  </SafeArea>
-);
+const Settings = () => {
+  const { onLogout } = useContext(AuthenticationContext);
+  return (
+    <SafeArea>
+      <Text>Settings</Text>
+      <Button title="logout" onPress={() => onLogout()} />
+    </SafeArea>
+  );
+};
 
 const createScreenOptions = ({ route }) => {
   const iconName = TAB_ICON[route.name];
