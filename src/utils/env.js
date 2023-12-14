6af3859d0acc6b775ea611ab96af5bf8ba3598e9
shibin-kurian <shibinkurian2013@gmail.com>
2023-12-15 00:24:52 +0530

    comments to fix local firebase http issue and instead live firebase function

--- src/utils/env.js
+++ src/utils/env.js
@@ -3,4 +3,4 @@ const localHost = "http://localhost:5001/mealstogo-b2612/us-central1";
 
 export const isDevelopment = process.env.NODE_ENV === "development";
 
-export const host = isDevelopment ? localHost : liveHost;
+export const host = /*isDevelopment ? localHost : */ liveHost;
